b1cb623431d89f39a7ffde2dea7c345c2d6f6c0a
Geoff Davis <gadavis@ucsd.edu>
2012-07-06 16:38:10 -0700

    Implement SSL support for Solaris

--- manifests/ssl/solaris.pp
+++ manifests/ssl/solaris.pp
@@ -1,5 +1,24 @@
-#class apache::ssl::solaris inherits apache::base::ssl {
-class apache::ssl::solaris {
-  # TODO: finish this class
-  notice('apache::ssl::solaris is a stub')
+class apache::ssl::solaris inherits apache::base::ssl {
+  file {"${apache::params::conf}/conf.d/ssl.conf":
+    ensure => absent,
+    require => Package["apache"],
+    notify => Service["apache"],
+    before => Exec["apache-graceful"],
+  }
+
+  apache::module { "ssl":
+    ensure => present,
+    require => File["${apache::params::conf}/conf.d/ssl.conf"],
+    notify => Service["apache"],
+    before => Exec["apache-graceful"],
+  }
+
+  file {"${apache::params::conf}/mods-available/ssl.load":
+    ensure => present,
+    content => template("apache/ssl.load.solaris.erb"),
+    mode => 644,
+    owner => "root",
+    group => "root",
+    require => File["${apache::params::conf}/mods-available"],
+  }
 }
